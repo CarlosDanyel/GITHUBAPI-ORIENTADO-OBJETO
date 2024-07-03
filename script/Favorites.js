81264a22158ac9613b1a4c9ff6d2fc8943018d9b
Carlos Danyel <carlosdanyelsilva27@gmail.com>
2024-07-03 15:22:19 -0300

    first commit

---
+++ script/Favorites.js
@@ -0,0 +1,125 @@
+export class GithubUser {
+  static seach(username) {
+    const endpoint = `https://api.github.com/users/${username}`;
+
+    return fetch(endpoint)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }));
+  }
+}
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+
+    GithubUser.seach("carlosdanyel").then((user) => console.log(user));
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+  }
+
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find((entry) => entry.login == username);
+
+      if (userExists) {
+        throw new Error("Usuario ja cadastrado");
+      }
+
+      const user = await GithubUser.seach(username);
+      if (user.login === undefined) {
+        throw new Error("Usuario nao encontrado");
+      }
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  delete(user) {
+    this.entries = this.entries.filter((entry) => entry.login !== user.login);
+    this.update();
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root);
+    this.tbody = this.root.querySelector("table tbody");
+    this.update();
+    this.onAdd();
+  }
+
+  onAdd() {
+    const addBtn = this.root.querySelector("#addBtn");
+    addBtn.onclick = () => {
+      const { value } = this.root.querySelector("#input_seach");
+      this.add(value);
+    };
+  }
+
+  update() {
+    this.removeAllTr();
+
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user p").textContent = user.name;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".repositorios").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que quer deletar esse cadastro?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+
+      this.tbody.append(row);
+    });
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/CarlosDanyel.png" alt="Imagem de Carlos Danyel"/>
+        <a href="https://github.com/CarlosDanyel" target="_blank">
+          <p>Carlos Danyel</p>
+          <span>/carlosdanyel</span>
+        </a>
+      </td>
+      <td class="repositorios">76</td>
+      <td class="followers">12000</td>
+      <td>
+        <button class="remove">Remover</button>
+      </td>
+    `;
+    return tr;
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
+  }
+}
